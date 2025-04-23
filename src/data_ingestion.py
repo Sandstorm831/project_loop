5481859fa99b77da7f985ed59bd751f250bdf1c7
Sandstorm831 <gargrohan138@gmail.com>
2025-04-23 12:48:32 +0530

    completed data_ingestion_script for creation of ingestor.db

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -13,7 +13,7 @@ def defineTables(cursor: Cursor):
     )''')
     print("created_timezones")
     cursor.execute('''CREATE TABLE IF NOT EXISTS store_pings (
-        id  INT PRIMARY KEY,
+        id  INTEGER PRIMARY KEY AUTOINCREMENT,
         store_id TEXT NOT NULL,
         is_active INT NOT NULL,
         recorded_at TEXT NOT NULL,
@@ -21,7 +21,7 @@ def defineTables(cursor: Cursor):
     )''')
     print("created_pings")
     cursor.execute('''CREATE TABLE IF NOT EXISTS store_hours (
-        id INT PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         store_id TEXT NOT NULL,
         week_day INTEGER NOT NULL,
         start_time TEXT NOT NULL,
@@ -39,9 +39,49 @@ def ingest_data(cursor: Cursor, conn: Connection):
     df_timezones = pl.scan_csv(timezones_path).collect()
     df_ping = pl.scan_csv(ping_path).collect()
     df_hours = pl.scan_csv(hours_path).collect()
-    df_timezone = df_timezones.
-    cursor.executemany("INSERT INTO store_timezones VALUES(?, ?, ?)", df)
-    conn.commit()
+    df_p = df_ping.select(
+        pl.col('store_id').unique(maintain_order=True).alias("UstoreId"),
+        pl.col('store_id').unique_counts().alias("count"),
+    )
+    df_h = df_hours.select(
+        pl.col('store_id').unique(maintain_order=True).alias('UstoreId'),
+        pl.col('store_id').unique_counts().alias("count"),
+    )
+    df_ph = df_p.vstack(df_h)
+    df_ph_uniq = df_ph.select(
+        pl.col('UstoreId').unique(maintain_order=True).alias('UstoreId'),
+        pl.col('UstoreId').unique_counts().alias("count"),
+    )
+    df_extra_timezones = df_ph_uniq.filter(
+        ~pl.col('UstoreId').is_in(df_timezones['store_id']),
+    )
+    df_proc = df_extra_timezones.select(
+        pl.col('UstoreId').alias('store_id'),
+        pl.lit('America/Chicago').alias('timezone_str'),
+    )
+    df_timezones.vstack(df_proc, in_place = True)
+    df_timezones = df_timezones.rename({"timezone_str": "timezone"})
+    df_ping = df_ping.select(
+        pl.col('store_id'),
+        pl.when(pl.col('status') == 'active').then(pl.lit(1)).otherwise(pl.lit(0)).alias('is_active'),
+        pl.col('timestamp_utc').alias('recorded_at'),
+    )
+    df_hours = df_hours.rename({"dayOfWeek": "week_day", "start_time_local": "start_time", "end_time_local": "end_time"})
+    df_timezones.write_database(
+        table_name="store_timezones",
+        connection=f'sqlite:///{db_loc}',
+        if_table_exists="append"
+    )
+    df_ping.write_database(
+        table_name="store_pings",
+        connection=f'sqlite:///{db_loc}',
+        if_table_exists="append",
+    )
+    df_hours.write_database(
+        table_name="store_hours",
+        connection=f'sqlite:///{db_loc}',
+        if_table_exists="append",
+    )
     return
 
 try:
@@ -49,7 +89,7 @@ try:
     cursor = conn.cursor()
 
     # define and create Tables
-    # defineTables(cursor)
+    defineTables(cursor)
 
     # Ingest data from csv files
     ingest_data(cursor, conn)
--- src/exp.py
+++ src/exp.py
@@ -1,28 +1,45 @@
 # import sqlite3
 import os
 import polars as pl
+from test.test_typing import Other
 # from sqlite3.dbapi2 import Connection, Cursor
 
 def tester():
     raw_path = os.path.abspath(__file__)
     timezones_path = raw_path.split("project_loop")[0] + "project_loop/data/timezones.csv"
-    # ping_path = raw_path.split("project_loop")[0] + "project_loop/data/store_status.csv"
+    ping_path = raw_path.split("project_loop")[0] + "project_loop/data/store_status.csv"
     hours_path = raw_path.split("project_loop")[0] + "project_loop/data/menu_hours.csv"
     df_timezones = pl.scan_csv(timezones_path).collect()
-    # df_ping = pl.scan_csv(ping_path).collect()
+    df_ping = pl.scan_csv(ping_path).collect()
     df_hours = pl.scan_csv(hours_path).collect()
-    # df_tp = df_hours.with_columns(
-    #     ~df_hours["store_id"].is_in(df_timezones['store_id']).alias("is_present"),
-    #     pl.lit("America/Chicago").alias('timezone')
+
+    # df_p = df_ping.select(
+    #     pl.col('store_id').unique(maintain_order=True).alias("UstoreId"),
+    #     pl.col('store_id').unique_counts().alias("count"),
     # )
-    df_tp = pl.DataFrame({
-    "store_id": df_hours["store_id"],
-    "is_present": df_hours["store_id"].is_in(df_timezones["store_id"]),
-    })
-    df_tp.write_csv("hello.csv")
-    print(df_tp)j
-    # cursor.executemany("INSERT INTO store_timezones VALUES(?, ?, ?)", df)
-    # conn.commit()
+    # df_h = df_hours.select(
+    #     pl.col('store_id').unique(maintain_order=True).alias('UstoreId'),
+    #     pl.col('store_id').unique_counts().alias("count"),
+    # )
+    # df_temp = df_p.vstack(df_h)
+    # df_uniq = df_temp.select(
+    #     pl.col('UstoreId').unique(maintain_order=True).alias('UstoreId'),
+    #     pl.col('UstoreId').unique_counts().alias("count"),
+    # )
+    # df_temp = df_uniq.filter(
+    #     ~pl.col('UstoreId').is_in(df_timezones['store_id']),
+    # )
+    # df_proc = df_temp.select(
+    #     pl.col('UstoreId').alias('store_id'),
+    #     pl.lit('America/Chicago').alias('timezone_str'),
+    # )
+    # df_timezones.vstack(df_proc, in_place = True)
+    df_ping = df_ping.select(
+        pl.col('store_id'),
+        pl.when(pl.col('status') == 'active').then(pl.lit(1)).otherwise(pl.lit(0)).alias('is_active'),
+        pl.col('timestamp_utc').alias('recorded_at'),
+    )
+    print(df_ping)
     return
 
 tester()
